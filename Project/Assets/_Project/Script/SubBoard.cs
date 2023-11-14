c9e931284ab89c7645f9adb5653dff10b83e92ae
Yunjae Na <dbsdbs2580@gmail.com>
2023-11-14 15:31:36 +0900

    2023.11.14

--- Project/Assets/_Project/Script/SubBoard.cs
+++ Project/Assets/_Project/Script/SubBoard.cs
@@ -8,10 +8,13 @@ public class SubBoard : MonoBehaviour
     private SpriteRenderer defaultImage;
     public Sprite ActivatedImage;
 
+    SubCharacter SubCharacter;
+
     // Start is called before the first frame update
     void Start()
     {
         defaultImage = GetComponent<SpriteRenderer>();
+        SubCharacter = GameObject.Find("SubCharacter").GetComponent<SubCharacter>();
     }
 
     // Update is called once per frame
@@ -23,23 +26,27 @@ public class SubBoard : MonoBehaviour
     // 충돌 감지
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if(this.gameObject.tag == "LeftBoard")
+            SubCharacter.Move_Left();
+        else if(this.gameObject.tag == "RightBoard")
+            SubCharacter.Move_Right();
+        else if(this.gameObject.tag == "UpBoard")
+            SubCharacter.Move_Up();
+        else if(this.gameObject.tag == "DownBoard")
+            SubCharacter.Move_Down();
+
         if(other.gameObject.tag == "MainCharacter")
             Activated();
-
-        
     }
 
     // 발판 활성화
     public void Activated() 
     {
-        Debug.Log(this.gameObject.tag);
-        
         // 이미지 변경
         defaultImage.sprite = ActivatedImage;
 
         // 태그 변경
         gameObject.tag = "ActivatedBoard";
-
     }
 
 }
--- Project/Assets/_Project/Script/SubCharacter.cs
+++ Project/Assets/_Project/Script/SubCharacter.cs
@@ -1,41 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SubCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 targetPosition;
+
     public float sum_X; // 좌우 이동 거리
     public float sum_Y; // 상하 이동 거리
+    private bool ladder_up = false;
+    private bool ladder_down = false;
+
+    GameManager gameManager;
+
+
     void Start()
     {
-        
+        targetPosition = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Move_Left()
     {
+        targetPosition -= new Vector3(sum_X, 0, 0);
+        transform.DOLocalMove(targetPosition, 0.25f);
 
     }
 
     public void Move_Right()
     {
-        
+        targetPosition += new Vector3(sum_X, 0, 0);
+        transform.DOLocalMove(targetPosition, 0.25f);
     }
 
     // 캐릭터가 사다리 오브젝트와 충돌되어있으면 올라감,내려감
     public void Move_Up()
     {
-        
+        targetPosition += new Vector3(0, sum_Y, 0);
+        transform.DOLocalMove(targetPosition, 0.25f);
     }
 
     public void Move_Down()
     {
-        
+        targetPosition -= new Vector3(0, sum_Y, 0);
+        transform.DOLocalMove(targetPosition, 0.25f);
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Goal")
+            Debug.Log("Success");
+        
+        if(other.gameObject.tag == "ladder_up")
+            ladder_up = true;
+
+        if(other.gameObject.tag == "ladder_down")
+            ladder_down = true;
+    }
+
 }
