bff3660c42a4782b529da4480c594034e58a04e1
BSF0124 <dbsdbs2580@gmail.com>
2024-02-14 15:30:32 +0900

    2024.02.14
    해상도 설정, 전체화면 설정 기능 구현

---
+++ PuzzleGame/Assets/Scripts/SelectMenu.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.IO;
+
+public class SelectMenu : MonoBehaviour
+{
+    public GameObject startPanel;
+    public GameObject optionPanel;
+    public TextMeshProUGUI[] slotText;
+
+    bool[] saveFile = new bool[3]; // true : 데이터 존재, false - 데이터 없음
+
+    void Start()
+    {
+        Refresh();
+    }
+    
+    // 데이터 새로고침
+    public void Refresh()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if(File.Exists($"{DataManager.instance.path}/save{i}.json"))
+            {
+                saveFile[i] = true;
+                DataManager.instance.currentSlot = i;
+                DataManager.instance.LoadData();
+
+                slotText[i].text = $"Player {i+1}";
+            }
+
+            else
+            {
+                saveFile[i] = false;
+                slotText[i].text = "Empty";
+            }
+        }
+
+        DataManager.instance.DataClear();
+    }
+
+    // 데이터 슬롯 선택 메서드
+    public void Slot(int num)
+    {
+        DataManager.instance.currentSlot = num;
+
+        if(saveFile[num])
+        {
+            DataManager.instance.LoadData();
+            StartCoroutine(GoGame());
+        }
+
+        else
+        {
+            DataManager.instance.SaveData();
+            StartCoroutine(GoGame());
+        }
+    }
+
+    // 데이터 선택창 표시
+    public void ShowStartPanel()
+    {
+        startPanel.gameObject.SetActive(true);
+    }
+
+    // 데이터 선택창 숨김
+    public void HideStartPanel()
+    {
+        startPanel.gameObject.SetActive(false);
+    }
+
+    // 옵션창 표시
+    public void ShowOptionPanel()
+    {
+        optionPanel.gameObject.SetActive(true);
+    }
+
+    // 옵션창 숨김
+    public void HideOptionPanel()
+    {
+        optionPanel.gameObject.SetActive(false);
+    }
+
+    // 데이터 삭제
+    public void Delete(int num)
+    {
+        DataManager.instance.currentSlot = num;
+        DataManager.instance.DeleteData();
+        Refresh();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // 씬 이동
+    private IEnumerator GoGame()
+    {
+        FadeManager.instance.FadeImage(0, 1, true);
+        yield return new WaitForSeconds(FadeManager.instance.imageDuration);
+        SceneManager.LoadScene("World");
+    }
+}
